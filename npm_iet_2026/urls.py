28e22eb1613069c4825954c54832a1bdf063fdb3
ArfalMalik <malikarfal27@gmail.com>
2026-03-15 22:58:16 +0700

    LabSession2

--- npm_iet_2026/urls.py
+++ npm_iet_2026/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
 
 def welcome(request):
@@ -8,4 +8,7 @@ def welcome(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('welcome/', welcome),
+    path('', include('main_app.urls')),
+    path('about/', include('about.urls')),
+    path('contacts/', include('contacts.urls')),
 ]
